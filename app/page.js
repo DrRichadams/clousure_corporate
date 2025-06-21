b7076d88d23c63fdaa58aae41e833a0ed6bb25ce
Richard <proxyserver7798@gmail.com>
2025-06-21 23:55:22 +0200

    End of shift commit

--- app/page.js
+++ app/page.js
@@ -1,10 +1,35 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Testimonials from "./components/testimonials";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <p>HELLO WORLD</p>
+      <header className={styles.header}>
+        <nav>
+          {/* <Image 
+            src="/assets/corporate-logo.png"
+            width={0} // required fallback
+            height={0} // required fallback
+            style={{ width: '1rem', height: 'auto' }}
+          /> */}
+          <img src="/assets/corporate-logo.png" />
+          <ul className={styles.nav_menus}>
+            <li>Portifolio</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+          </ul>
+        </nav>
+        <Image 
+          src="/assets/banner.png" 
+          alt="Banner" 
+          fill
+          style={{ objectFit: 'contain' }}
+        />
+      </header>
+      <main>
+        <Testimonials />
+      </main>
     </div>
   );
 }
